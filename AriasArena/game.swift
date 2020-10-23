1a6b07a91281fdd2ca2292c5878be4a32d6d6a6a
Johann Ulma <johann.ulma@gmail.com>
2020-10-23 11:05:20 +0200

    Add Main File
    Repair mistake after forgot Main file

--- AriasArena/game.swift
+++ AriasArena/game.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class Game {
+    let round: Int
+    let player1: Player
+    let player2: Player
+    let care: Int
+    
+    init(round: Int, player1: Player, player2: Player, care: Int){
+        self.round = round
+        self.player1 = player1
+        self.player2 = player2
+        self.care = care
+    }
+    func RunGame() {
+
+    }
+}
--- AriasArena/weapon.swift
+++ AriasArena/weapon.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class Weapon {
+    let name: String
+    let type: String
+    let damage: Int
+    let care: Int
+    
+    init(name: String, type: String, damage: Int, care: Int){
+        self.name = name
+        self.type = type
+        self.damage = damage
+        self.care = care
+    }
+    func attack () {
+        print("Make \(damage) points damages")
+    }
+    func healing () {
+        print("Healing for \(care) points")
+    }
+}
