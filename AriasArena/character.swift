27a3370720cf39cda9b4ae19dd1adcfbcaa66cdd
Johann Ulma <johann.ulma@gmail.com>
2020-10-26 10:06:56 +0100

    Updating of Classes
    Updated variables and functions for character, game and player

--- AriasArena/character.swift
+++ AriasArena/character.swift
@@ -11,11 +11,17 @@ class Character {
     let name: String
     var life: Int
     var weapon: Weapon
+    var canHeal: Bool
+    var canAttack: Bool
     
-    init(name: String, life: Int, weapon:Weapon){
+    init(name: String, life: Int, weapon:Weapon, canHeal: Bool, canAttack: Bool){
         self.name = name
         self.life = life
         self.weapon = weapon
+        self.canHeal = canHeal
+        self.canAttack = canAttack
+    }
+    func action() {
         
     }
     func lifeupdate () {
